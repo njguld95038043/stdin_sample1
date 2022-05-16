1ab2bea8ef8e8f56ed7557eddd1d5b1b7fbd3dde
njguld95038043 <t.tamura6060@gmail.com>
2022-05-16 03:40:06 +0000

    [Update]air_conditioner2.rb

--- air_conditioner2.rb
+++ air_conditioner2.rb
@@ -1,18 +1,10 @@
-input_lines = readlines
-
-room = input_lines[0].to_i
-air_conditioner = input_lines[1].to_i
-
-temperature_diff = (room - air_conditioner).abs
-
-require_time = 0
-
-if temperature_diff < 5
-    require_time = 15
-elsif temperature_diff >= 5 && temperature_diff < 10 then
-    require_time = 30
-elsif temperature_diff >= 10  then
-    require_time = 60
+num = []
+sum = 0
+count = 0
+while sum < 15
+  count += 1
+  reciprocal = 1.0/count
+  num << reciprocal
+  sum = num.sum
 end
-
-p require_time
+puts num.length
